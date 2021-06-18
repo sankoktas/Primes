4300621cf4207abcffd624e677472ccd11b8d5bf
Fikri San Koktas <kokfik.21@robcol.k12.tr>
2021-06-18 22:44:03 +0300

    Update and rename README.md to code.java

---
+++ code.java
@@ -0,0 +1,26 @@
+/**This program calculates PI 
+ * @author (Fikri San Koktas)
+ * @version (18 February 2021)*/
+import java.util.Scanner;
+public class KFS_Primes_Main
+{
+    public static void main(String[] args)
+    {
+        int n;
+        int p;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        n=s.nextInt();
+        for(int i=2; i<n; i++)
+        {
+            p=0;
+            for(int j=2; j<i; j++)
+            {
+                if(i%j == 0)
+                    p=1;
+            }
+            if(p == 0)
+                System.out.println(i); 
+        }
+    }
+}
